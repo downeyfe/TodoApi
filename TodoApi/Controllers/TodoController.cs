3000c076ac7e54bc5ec04e1d2ab661d059939b27
Frances Downey <downeyfe@gmail.com>
2017-09-19 17:00:19 +0100

    Adding get endpoints for todos

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -19,6 +19,24 @@ namespace TodoApi.Controllers
                 _context.TodoItems.Add(new TodoItem { Name = "item1" });
                 _context.SaveChanges();
             }
+
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
         }
     }
 }
